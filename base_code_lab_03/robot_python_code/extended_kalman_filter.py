7edd0ae0315e78f0bde3b351b1217301a79e9428
haizhou <haizhou_zhao@outlook.com>
2026-02-25 17:23:14 -0500

    add ekf

--- base_code_lab_03/robot_python_code/extended_kalman_filter.py
+++ base_code_lab_03/robot_python_code/extended_kalman_filter.py
@@ -8,68 +8,110 @@ from matplotlib.patches import Ellipse
 import parameters
 import data_handling
 
+from motion_models import MyMotionModel
+
+
 # Main class
 class ExtendedKalmanFilter:
     def __init__(self, x_0, Sigma_0, encoder_counts_0):
         self.state_mean = x_0
         self.state_covariance = Sigma_0
-        self.predicted_state_mean = [0,0,0]
+        self.predicted_state_mean = [0, 0, 0]
         self.predicted_state_covariance = parameters.I3 * 1.0
         self.last_encoder_counts = encoder_counts_0
+        self.model = MyMotionModel(x_0, encoder_counts_0)
+        self.model.step_with_noise = False  # EKF prediction step should not have noise, since we account for noise in the R matrix
+        self.model.return_noise_scale = True
 
     # Call the prediction and correction steps
     def update(self, u_t, z_t, delta_t):
+        """
+        u_t is not real u but encoder_counts reading and steering command, since we need encoder counts to calculate distance travelled in the motion model
+        z_t is camera measurement of x, y, theta
+        """
+        self.prediction_step(u_t, delta_t)
+        self.correction_step(z_t)
         return
 
     # Set the EKF's predicted state mean and covariance matrix
     def prediction_step(self, u_t, delta_t):
+        self.model.state = self.state_mean # sync states
+        self.model.last_encoder_count = self.last_encoder_counts # sync encoder counts for distance calculation
+        x_tp_mean, s = self.model.step_update(u_t[0], u_t[1], delta_t)
+        G_x = self.get_G_x(self.state_mean, s, delta_t)
+        R_t = self.get_R(s)
+        G_u = self.get_G_u(self.state_mean, delta_t)
+        self.predicted_state_mean = x_tp_mean
+        self.predicted_state_covariance = np.matmul(
+            np.matmul(G_x, self.state_covariance), G_x.T
+        ) + np.matmul(np.matmul(G_u, R_t), G_u.T)
+        self.last_encoder_counts = u_t[0]
         return
 
     # Set the EKF's corrected state mean and covariance matrix
     def correction_step(self, z_t):
-        return
+        H_t = self.get_H()
+        Q_t = self.get_Q()
+        S_t = H_t @ self.predicted_state_covariance @ H_t.T + Q_t
+        K_t = self.predicted_state_covariance @ H_t.T @ np.linalg.inv(S_t)
+        diff = z_t - self.get_h_function(self.predicted_state_mean)
 
-    # Function to calculate distance from encoder counts
-    def distance_travelled_s(self, encoder_counts):
-        return 0    
-            
-    # Function to calculate rotational velocity from steering and dist travelled or speed
-    def rotational_velocity_w(self, steering_angle_command):        
-        return 0
+        self.state_mean = self.predicted_state_mean + K_t @ diff
+        self.state_covariance = (parameters.I3 - K_t @ H_t) @ self.predicted_state_covariance
+        return
 
     # The nonlinear transition equation that provides new states from past states
     def g_function(self, x_tm1, u_t, delta_t):
-        x_t = x_tm1
-        s = 0
-        return x_t, s
-    
+        raise NotImplementedError(
+            "use self.model.step_update instead of g_function for prediction step since we need noise scale from motion model for EKF"
+        )
+
     # The nonlinear measurement function
     def get_h_function(self, x_t):
         return x_t
-    
+
     # This function returns a matrix with the partial derivatives dg/dx
-    # g outputs x_t, y_t, theta_t, and we take derivatives wrt inputs x_tm1, y_tm1, theta_tm1
-    def get_G_x(self, x_tm1, s):       
-        return parameters.I3
+    def get_G_x(self, x_tm1, s, delta_t):
+        G = np.array(
+            [
+                [1, 0, -self.model.distance_step * math.sin(self.model.state[2])],
+                [0, 1, self.model.distance_step * math.cos(self.model.state[2])],
+                [0, 0, self.model.w_Rad * delta_t],
+            ]
+        )
+        return G
 
     # This function returns a matrix with the partial derivatives dg/du
-    def get_G_u(self, x_tm1, delta_t):                
-        return parameters.I3
+    def get_G_u(self, x_tm1, delta_t):
+        return np.array(
+            [
+                [math.cos(self.model.state[2]), 0],
+                [math.sin(self.model.state[2]), 0],
+                [0, delta_t],
+            ]
+        )
 
     # This function returns a matrix with the partial derivatives dh_t/dx_t
     def get_H(self):
         return parameters.I3
-    
+
     # This function returns the R_t matrix which contains transition function covariance terms.
     def get_R(self, s):
-        return parameters.I3
+        # Scale process noise with distance travelled or a baseline
+        return np.diag(s)
 
     # This function returns the Q_t matrix which contains measurement covariance terms.
     def get_Q(self):
-        return parameters.I3
+        return np.diag(
+            [
+                0.0048478566654710465,
+                0.007572909242886893,
+                0.009776259224975919,
+            ]
+        )
+
 
 class KalmanFilterPlot:
-
     def __init__(self):
         self.dir_length = 0.1
         fig, ax = plt.subplots()
@@ -83,16 +125,27 @@ class KalmanFilterPlot:
         lambda_, v = np.linalg.eig(state_covaraiance)
         lambda_ = np.sqrt(lambda_)
         xy = (state_mean[0], state_mean[1])
-        angle=np.rad2deg(np.arctan2(*v[:,0][::-1]))
-        ell = Ellipse(xy, alpha=0.5, facecolor='red',width=lambda_[0], height=lambda_[1], angle = angle)
+        angle = np.rad2deg(np.arctan2(*v[:, 0][::-1]))
+        ell = Ellipse(
+            xy,
+            alpha=0.5,
+            facecolor="red",
+            width=lambda_[0],
+            height=lambda_[1],
+            angle=angle,
+        )
         ax = self.fig.gca()
         ax.add_artist(ell)
-        
+
         # Plot state estimate
-        plt.plot(state_mean[0], state_mean[1],'ro')
-        plt.plot([state_mean[0], state_mean[0]+ self.dir_length*math.cos(state_mean[2]) ], [state_mean[1], state_mean[1]+ self.dir_length*math.sin(state_mean[2]) ],'r')
-        plt.xlabel('X(m)')
-        plt.ylabel('Y(m)')
+        plt.plot(state_mean[0], state_mean[1], "ro")
+        plt.plot(
+            [state_mean[0], state_mean[0] + self.dir_length * math.cos(state_mean[2])],
+            [state_mean[1], state_mean[1] + self.dir_length * math.sin(state_mean[2])],
+            "r",
+        )
+        plt.xlabel("X(m)")
+        plt.ylabel("Y(m)")
         plt.axis([-0.25, 2, -1, 1])
         plt.grid()
         plt.draw()
@@ -103,11 +156,11 @@ class KalmanFilterPlot:
 def offline_efk():
 
     # Get data to filter
-    filename = './data/robot_data_68_0_06_02_26_17_12_19.pkl'
+    filename = "./data/robot_data_68_0_06_02_26_17_12_19.pkl"
     ekf_data = data_handling.get_file_data_for_kf(filename)
 
     # Instantiate PF with no initial guess
-    x_0 = [ekf_data[0][3][0]+.5, ekf_data[0][3][1], ekf_data[0][3][5]]
+    x_0 = [ekf_data[0][3][0] + 0.5, ekf_data[0][3][1], ekf_data[0][3][5]]
     Sigma_0 = parameters.I3
     encoder_counts_0 = ekf_data[0][2].encoder_counts
     extended_kalman_filter = ExtendedKalmanFilter(x_0, Sigma_0, encoder_counts_0)
@@ -118,15 +171,18 @@ def offline_efk():
     # Loop over sim data
     for t in range(1, len(ekf_data)):
         row = ekf_data[t]
-        delta_t = ekf_data[t][0] - ekf_data[t-1][0] # time step size
-        u_t = np.array([row[2].encoder_counts, row[2].steering]) # robot_sensor_signal
-        z_t = np.array([row[3][0],row[3][1],row[3][5]]) # camera_sensor_signal
+        delta_t = ekf_data[t][0] - ekf_data[t - 1][0]  # time step size
+        u_t = np.array([row[2].encoder_counts, row[2].steering])  # robot_sensor_signal
+        z_t = np.array([row[3][0], row[3][1], row[3][5]])  # camera_sensor_signal
 
         # Run the EKF for a time step
         extended_kalman_filter.update(u_t, z_t, delta_t)
-        kalman_filter_plot.update(extended_kalman_filter.state_mean, extended_kalman_filter.state_covariance[0:2,0:2])
+        kalman_filter_plot.update(
+            extended_kalman_filter.state_mean,
+            extended_kalman_filter.state_covariance[0:2, 0:2],
+        )
 
 
 ####### MAIN #######
-if False:
+if True:
     offline_efk()
--- base_code_lab_03/robot_python_code/motion_models.py
+++ base_code_lab_03/robot_python_code/motion_models.py
@@ -1,52 +1,130 @@
 # External Libraries
 import math
 import random
+import numpy as np
 
 # Motion Model constants
 
 
 # A function for obtaining variance in distance travelled as a function of distance travelled
-def variance_distance_travelled_s(distance):
+def variance_distance_travelled_s(encoder_counts):
     # Add student code here
-    var_s = 1
+    a = -5.38659836735178e-08
+    b = 0.0002806804940722607
+    c = 0.1051219368000114
 
-    return var_s
+    var_s = a * encoder_counts**2 + b * encoder_counts + c
+
+    return var_s * 0.01**2  # convert to meters^2
+
 
 # Function to calculate distance from encoder counts
 def distance_travelled_s(encoder_counts):
     # Add student code here
-    s = 0
+    s = 0.028308198909415782 * encoder_counts
 
-    return s
+    return s * 0.01  # convert to meters
+
 
 # A function for obtaining variance in distance travelled as a function of distance travelled
-def variance_rotational_velocity_w(distance):
+def variance_rotational_velocity_w(steering_angle_command, speed_command):
     # Add student code here
-    var_w = 1
+    c = 0.9126683501683492
+    var_w = c
 
     return var_w
 
-def rotational_velocity_w(steering_angle_command):
+
+def rotational_velocity_w(steering_angle_command, speed_command):
     # Add student code here
-    w = 0
-    
+    def model_rot(xdata, a, b, c, d, e, f):
+        return (
+            a * xdata[0]
+            # + b * xdata[1]
+            # + c * xdata[0] ** 2
+            # + d * xdata[1] ** 2
+            # + e * xdata[0] * xdata[1]
+            + c * xdata[0] ** 3 * xdata[1]
+            # + d * xdata[0] ** 2 * xdata[1]
+            + e * xdata[0] * xdata[1]
+            + f
+        )
+
+    p = [
+        1.57500000e00,
+        1.00000000e00,
+        -8.88888889e-05,
+        1.00000000e00,
+        2.77777778e-04,
+        -1.28205128e-01,
+    ]
+    # p = [-0.59469697, 0.10597015, -0.00808458, -0.00160586, 0.03744949]
+    w = model_rot((steering_angle_command, speed_command), *p)
+
     return w
+
 
 # This class is an example structure for implementing your motion model.
 class MyMotionModel:
-
     # Constructor, change as you see fit.
     def __init__(self, initial_state, last_encoder_count):
         self.state = initial_state
         self.last_encoder_count = last_encoder_count
+        self.step_with_noise = True
+        self.gen_noise = False
+        self.return_noise_scale = False
 
     # This is the key step of your motion model, which implements x_t = f(x_{t-1}, u_t)
     def step_update(self, encoder_counts, steering_angle_command, delta_t):
         # Add student code here
-        
-        return self.state
-    
-    # This is a great tool to take in data from a trial and iterate over the data to create 
+        distance = distance_travelled_s(encoder_counts - self.last_encoder_count)
+        if self.step_with_noise:
+            self.gen_noise = True
+        if self.gen_noise or self.return_noise_scale:
+            dist_std_err = math.sqrt(
+                variance_distance_travelled_s(encoder_counts - self.last_encoder_count)
+            )
+        if self.gen_noise:
+            dist_noise = random.normalvariate(0, dist_std_err)
+        if self.step_with_noise:
+            distance += dist_noise
+        self.distance_step = distance  # Store the distance step for use in the Jacobian
+        self.last_encoder_count = encoder_counts
+        # our tested cmd to actual speed ratio is speed / cmd = 0.42
+        est_vel = distance / delta_t / 0.42 * 100
+        w = rotational_velocity_w(steering_angle_command, est_vel)
+        if self.gen_noise or self.return_noise_scale:
+            w_std_err = (
+                math.sqrt(
+                    max(
+                        0,
+                        variance_rotational_velocity_w(steering_angle_command, est_vel),
+                    )
+                ),
+            )
+
+        if self.gen_noise:
+            w_noise = np.sign(w) * random.normalvariate(0, w_std_err)
+        if self.step_with_noise:
+            w += w_noise
+        self.w_Rad = w / 180 * math.pi
+        self.state[2] = self.state[2] + self.w_Rad * delta_t
+        self.state[0] = self.state[0] + distance * math.cos(self.state[2])
+        self.state[1] = self.state[1] + distance * math.sin(self.state[2])
+
+        if self.return_noise_scale:
+            x_noise_scale = np.array(
+                [
+                    dist_std_err * math.cos(self.state[2]),
+                    dist_std_err * math.sin(self.state[2]),
+                    w_std_err / 180 * math.pi * delta_t,
+                ]
+            )
+            return self.state, x_noise_scale
+        else:
+            return self.state
+
+    # This is a great tool to take in data from a trial and iterate over the data to create
     # a robot trajectory in the global frame, using your motion model.
     def traj_propagation(self, time_list, encoder_count_list, steering_angle_list):
         x_list = [self.state[0]]
@@ -54,26 +132,33 @@ class MyMotionModel:
         theta_list = [self.state[2]]
         self.last_encoder_count = encoder_count_list[0]
         for i in range(1, len(encoder_count_list)):
-            delta_t = time_list[i] - time_list[i-1]
-            new_state = self.step_update(encoder_count_list[i], steering_angle_list[i], delta_t)
+            delta_t = time_list[i] - time_list[i - 1]
+            new_state = self.step_update(
+                encoder_count_list[i], steering_angle_list[i], delta_t
+            )
             x_list.append(new_state[0])
             y_list.append(new_state[1])
             theta_list.append(new_state[2])
 
         return x_list, y_list, theta_list
-    
 
     # Coming soon
-    def generate_simulated_traj(self, duration):
+    def generate_simulated_traj(self, duration, steering_angle):
         delta_t = 0.1
         t_list = []
-        x_list = []
-        y_list = []
-        theta_list = []
+        x_list = [self.state[0]]
+        y_list = [self.state[1]]
+        theta_list = [self.state[2]]
         t = 0
         encoder_counts = 0
         while t < duration:
-
-            t += delta_t 
-        return t_list, x_list, y_list, theta_list
-            
+            # 1000 -> 30cm
+            # speed 5cm /s -> 166.67 encoder counts / s
+            encoder_counts += int(166.67 * delta_t)
+            self.step_update(encoder_counts, steering_angle, delta_t)
+            x_list.append(self.state[0])
+            y_list.append(self.state[1])
+            theta_list.append(self.state[2])
+            t += delta_t
+            t_list.append(t)
+        return x_list, y_list, theta_list
